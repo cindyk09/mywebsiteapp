580a8909d5dac406451bb7a6867806f3547226d7
cindyk09 <cindy_kim88@hotmail.com>
2016-03-03 11:56:54 -0500

    added email column to admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160302235535) do
+ActiveRecord::Schema.define(version: 20160303164958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160302235535) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "email"
   end
 
   create_table "projects", force: :cascade do |t|
