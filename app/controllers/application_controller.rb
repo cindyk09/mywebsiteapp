2f56d07882e77af259961c582b2b3eb3861ab8f0
cindyk09 <cindy_kim88@hotmail.com>
2016-03-08 16:05:21 -0500

    fixed header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :current_uri
+
+  # def current_uri(@project)
+  #   request.env['PATH_INFO'] != '/projects/:id'
+  # end
+
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
