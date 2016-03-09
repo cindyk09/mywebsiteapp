11914a7e8969380da616dbbaee4a52295dcea5ec
cindyk09 <cindy_kim88@hotmail.com>
2016-03-09 17:34:53 -0500

    took out helper method line

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,5 @@ module ApplicationHelper
 
   def show_plain_nav_bar
     CONTROLLERS.include?(controller.controller_name) && ACTIONS.include?(controller.action_name)
-    # controller.controller_name.include?(CONTROLLERS) && controller.action_name.include?(ACTIONS)
   end
 end
