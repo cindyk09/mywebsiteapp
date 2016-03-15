783d61f01efe804f0b74d471ee2a15d729692a16
cindyk09 <cindy_kim88@hotmail.com>
2016-03-15 17:02:28 -0400

    log in flash and form fixed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,18 +6,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-      user = User.authenticate!(params[:email], params[:password])
+    binding.pry
+      user = User.authenticate!(params[:user][:email], params[:user][:password])
       if user
         session[:user_id] = user.id
         redirect_to user_path(user)
       else
-        flash[:error] = "Bad e-mail or password"
+        flash[:error] = "Bad E-mail or Password"
         redirect_to sessions_path
       end
     end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: "You have been logged out!"
+    redirect_to root_path, loggedout: "You have been logged out!"
   end
 end
