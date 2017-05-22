932169b0af29ae481605ce0ace3b4ed6bc11ff56
cindyk09 <cindyk09@gmail.com>
2017-05-22 12:04:21 -0400

    adjusted upload size for large on images

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :image_upload, styles: { large: "600x500#", medium: "300x250#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image_upload, styles: { large: "600x500^", medium: "300x250#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image_upload, content_type: /\Aimage\/.*\Z/
 end
